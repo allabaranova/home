a282858be194a75ddfdbaa7dcd2dca03c737889f
Alla.Baranova <alla.baranova@ctco.lv>
2018-04-17 20:35:26 +0300

    QuizQuestionRepository - redundant query removed

--- main/java/es/nitaur/repository/QuizQuestionRepository.java
+++ main/java/es/nitaur/repository/QuizQuestionRepository.java
@@ -2,8 +2,6 @@ package es.nitaur.repository;
 
 import es.nitaur.domain.QuizQuestion;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,6 +9,5 @@ import java.util.List;
 @Repository
 public interface QuizQuestionRepository extends JpaRepository<QuizQuestion, Long> {
 
-    @Query("SELECT qq from QuizQuestion qq where qq.section.id = :id")
-    List<QuizQuestion> findAllBySectionId(@Param("id") Long filterId);
+    List<QuizQuestion> findAllBySectionId(Long filterId);
 }
